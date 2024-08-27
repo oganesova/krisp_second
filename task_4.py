40f21012f4c5238e29dd3a03f543257153a447a0
oganesova <91662679+oganesova@users.noreply.github.com>
2024-08-27 22:15:37 +0400

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -10,8 +10,6 @@ def stream_payments(callback_fn):
     Reads payments from a payment processor and calls `callback_fn(amount)` for each payment.
     Returns when there is no more payments.
     """
-    # Sample implementation to make the code run in coderpad.
-    # Do not rely on this exact implementation.
     for i in range(10):
         callback_fn(i)
 
@@ -20,8 +18,6 @@ def store_payments(amount_iterator):
     Iterates over the payment amounts from amount_iterator
     and stores them to a remote system.
     """
-    # Sample implementation to make the code run in coderpad.
-    # Do not rely on this exact implementation.
     for i in amount_iterator:
         print(i)
 
